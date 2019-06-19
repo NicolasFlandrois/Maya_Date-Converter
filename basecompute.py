fb7002a6311bc6060cb4a4b486e8f6cc4b86dd77
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-19 17:08:46 +0200

    Updated the comments and Todos
    Also converted all indents to 4 spaces, getting ready for PEP8

--- basecompute.py
+++ basecompute.py
@@ -4,33 +4,33 @@
 # Author: Nicolas Flandrois
 
 class Base(object):
-	"""Base class group functions to compute a number from base 10
-	to n-base. As needed for a Mayan Long Count, the output will still be
-	displayed using arabic numbers 0-9 (No Letters as number here)."""
-		
-	def base10toN(n_input, n_base):
-		"""This function will compute from Base-10 to Base-N"""
-		a = n_input//n_base
-		b = n_input%n_base
-		return a, b 
-		# Returns a Tuple (a, b) ('a' (Tiple[0]) will be compute for the next digit, 
-		# 'b' (Tiple[1]) is the reminder)
+    """Base class group functions to compute a number from base 10
+    to n-base. As needed for a Mayan Long Count, the output will still be
+    displayed using arabic numbers 0-9 (No Letters as number here)."""
+        
+    def base10toN(n_input, n_base):
+        """This function will compute from Base-10 to Base-N"""
+        a = n_input//n_base
+        b = n_input%n_base
+        return a, b 
+        # Returns a Tuple (a, b) ('a' (Tiple[0]) will be compute for the next digit, 
+        # 'b' (Tiple[1]) is the reminder)
 
-	def baseNto10(n_input, n_base):	
-		"""This function will compute from Base-N to Base-10"""
-		a = n_input*n_base
-	# 	b = n_input%n_base #?????
-		return a # , b # Returns a Tuple (a, b)
+    def baseNto10(n_input, n_base): 
+        """This function will compute from Base-N to Base-10"""
+        a = n_input*n_base
+    #   b = n_input%n_base #?????
+        return a # , b # Returns a Tuple (a, b)
 
 # Class Translation():
 # Using the following computation.
 # 9.12.2.0.16 (Long Count)
-# 	9	× 144000	= 1296000
-# 	12	× 7200	= 86400
-# 	2	× 360	= 720
-# 	0	× 20	= 0
-# 	16	× 1	= 16
-# 	Total days	= 1383136
+#   9   × 144000    = 1296000
+#   12  × 7200  = 86400
+#   2   × 360   = 720
+#   0   × 20    = 0
+#   16  × 1 = 16
+#   Total days  = 1383136
 
 #TEST
 # print("base10toN")
--- datecompute.py
+++ datecompute.py
@@ -6,28 +6,31 @@
 import datetime
 
 class Convert(object):
-	"""Convert will regroup functions managing dates."""
-	
-	def nowdate():
-		"""Date now"""
-		return datetime.date.today()
+    """Convert will regroup functions managing dates."""
+    
+    def nowdate():
+        """Date now"""
+        return datetime.date.today()
 
-	def inputdate(year, month, day):
-		"""Formating the date into a computable format."""
-		return datetime.date(year, month, day)
+    def inputdate(year, month, day):
+        """Formating the date into a computable format."""
+        return datetime.date(year, month, day)
 
-	def convertor(datedata):
-		"""Issuing the date now as a decimal integer ready to be converted 
-		into Mayan count bases (Base-20 and Base-18)"""
-		ref_a = datetime.date(1970, 1, 1) 
-			# Choosen as ref point, as this date is Unix Time = 0
-		ref_b = 1856305 #Long Count Base(20): 0.0.0.0.0.12.17.16.7.5 
-			# which can be translated into base(20) = CHG75
-			# Decimal (base 10) value of maya long count's ref point
-		delta = datedata - ref_a
-			# Decimal value of maya long count for today
-			# Then convert this decimal into base(20)
-		return ref_b + delta.days
+    def convertor(datedata):
+        """Issuing the date now as a decimal integer ready to be converted 
+        into Mayan count bases (Base-20 and Base-18)"""
+        ref_a = datetime.date(1970, 1, 1) 
+            # Choosen as ref point, as this date is Unix Time = 0
+        ref_b = 1856305 #Long Count Base(20): 0.0.0.0.0.12.17.16.7.5 
+            # which can be translated into base(20) = CHG75
+            # Decimal (base 10) value of maya long count's ref point
+        delta = datedata - ref_a
+            # Decimal value of maya long count for today
+            # Then convert this decimal into base(20)
+        return ref_b + delta.days
+
+# class Translator(object):
+#   """Translator Translating a long count decimal number into a Date."""
 
 # Test
 # print("Computing Today's date Automaticaly")
@@ -36,4 +39,4 @@ class Convert(object):
 # print("Computing an Input Date")
 # print("Formating the date : ", Convert.inputdate(1970, 7, 2))
 # print("Converting the date : ", Convert.convertor(
-# 	  Convert.inputdate(1970, 7, 2)))
+#     Convert.inputdate(1970, 7, 2)))
