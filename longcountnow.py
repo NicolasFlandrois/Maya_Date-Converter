4d628af52e81c3319a53d92e7953a5144f823430
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-19 15:26:25 +0200

    Changed the Date count into a Method Static Class & Function

--- longcountnow.py
+++ longcountnow.py
@@ -5,15 +5,19 @@
 
 import datetime
 
-ref_a = datetime.date(1970, 1, 1) 
-#Choosen as ref point, as this date is Unix Time = 0
-ref_b = 2062545 #Long Count Base(20): 0.0.0.0.0.12.17.16.7.5 
-#which can be translated into base(20) = CHG75
-#Decimal (base 10) value of maya long count's ref point
-today = datetime.date.today()
-delta = today - ref_a
-#Decimal value of maya long count for today
-dec_lc_today = ref_b + delta.days
-#Then convert this decimal into base(20)
-print(dec_lc_today)
-#However the long count system isn't entirely a base(20) > cf notes
+class DateCount(object):
+	"""docstring for DateCount"""
+		
+	def datenow():
+		"""Docstring"""
+		ref_a = datetime.date(1970, 1, 1) 
+		#Choosen as ref point, as this date is Unix Time = 0
+		ref_b = 2062545 #Long Count Base(20): 0.0.0.0.0.12.17.16.7.5 
+		#which can be translated into base(20) = CHG75
+		#Decimal (base 10) value of maya long count's ref point
+		today = datetime.date.today()
+		delta = today - ref_a
+		#Decimal value of maya long count for today
+		dec_lc_today = ref_b + delta.days
+		#Then convert this decimal into base(20)
+		return dec_lc_today
