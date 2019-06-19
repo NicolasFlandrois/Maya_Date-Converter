d3d1b35ef45993867aa7f8866f27bf6b0ec0d621
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-19 15:12:02 +0200

    Merged into a  function single function, passing base in argument
    1 function to convert from base 10 to Base n
    1 function to translate from base n to base 10

--- basecompute.py
+++ basecompute.py
@@ -2,32 +2,24 @@ class Base(object):
 	"""Base class group functions to compute a number from base 10 
 	to n-base"""
 		
-	def base10to20(number):
-		"""This function will compute from Base-10 to Base-20"""
-		a = number//20
-		b = number%20
+	def base10toN(n_input, n_base):
+		"""This function will compute from Base-10 to Base-N"""
+		a = n_input//n_base
+		b = n_input%n_base
 		return a, b 
 		# Returns a Tuple (a, b) ('a' will be compute for the next digit, 
 		# 'b' is the reminder)
 
-	def base10to18(number):
-			"""This function will compute from Base-10 to Base-18"""
-			a = number//18
-			b = number%18
-			return a, b # Returns a Tuple
-
-	# def base18to10(number):	
-	# 	"""This function will compute from Base-18 to Base-10"""
-	# 	a = number//xyz
-	# 	b = number%xyz
-	# 	return a, b # Returns a Tuple
-
-	# def base20to10(number):	
-	# 	"""This function will compute from Base-20 to Base-10"""
-	# 	a = number//xyz
-	# 	b = number%xyz
-	# 	return a, b # Returns a Tuple
+	def baseNto10(n_input, n_base):	
+		"""This function will compute from Base-N to Base-10"""
+		a = n_input*n_base
+	# 	b = n_input%n_base #?????
+		return a # , b # Returns a Tuple (a, b)
 
 #TEST
-print(Base.base10to20(50))
-print(Base.base10to18(50))
+print("base10toN")
+print("Base 20: ", Base.base10toN(50, 20))
+print("Base 18: ", Base.base10toN(50, 18))
+print("baseNto10")
+print("Base 20: ", Base.baseNto10(60, 20))
+print("Base 18: ", Base.baseNto10(40, 18))
