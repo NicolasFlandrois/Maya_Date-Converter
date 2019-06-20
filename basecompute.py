e105b1df52b6c740e975112c5a4de3ebff71d643
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-20 14:20:33 +0200

    Created a Translator from a MLC dictionary to a MLC decimal date Integer
    Mayan Long Count

--- basecompute.py
+++ basecompute.py
@@ -13,24 +13,39 @@ class Base(object):
         a = n_input//n_base
         b = n_input%n_base
         return a, b 
-        # Returns a Tuple (a, b) ('a' (Tiple[0]) will be compute for the next digit, 
-        # 'b' (Tiple[1]) is the reminder)
+        # Returns a Tuple (a, b) ('a' (Tiple[0]) will be compute for the next
+        # digit, 'b' (Tiple[1]) is the reminder)
 
-    def baseNto10(n_input, n_base): 
-        """This function will compute from Base-N to Base-10"""
-        a = n_input*n_base
-    #   b = n_input%n_base #?????
-        return a # , b # Returns a Tuple (a, b)
+    def mlctobase10(mlc:dict):
+        """This functions to compute a MayanLC dictionary, to a MayanLC 
+        Decimal Integer.
 
-# Class Translation():
-# Using the following computation.
-# 9.12.2.0.16 (Long Count)
-#   9   × 144000    = 1296000
-#   12  × 7200  = 86400
-#   2   × 360   = 720
-#   0   × 20    = 0
-#   16  × 1 = 16
-#   Total days  = 1383136
+        mlc = {"kin":0, "winal":0, "tun":0, "katun":0, "baktun":0, "piktun":0,
+               "kalabtun":0, "kinchiltun":0, "alautun":0, "alautun_rest":0}
+
+        Using the following computation.
+        9.12.2.0.16 (Long Count)
+          9   × 144000    = 1296000
+          12  × 7200  = 86400
+          2   × 360   = 720
+          0   × 20    = 0
+          16  × 1 = 16
+          Total (sum) days  = 1383136"""
+
+        kin = mlc["kin"]*1
+        winal = mlc["winal"]*20
+        tun = mlc["tun"]*360
+        katun = mlc["katun"]*7200
+        baktun = mlc["baktun"]*144000
+        piktun = mlc["piktun"]*2880000
+        kalabtun = mlc["kalabtun"]*57600000
+        kinchiltun = mlc["kinchiltun"]*1152000000
+        alautun = mlc["alautun"]*23040000000
+        alautun_rest = mlc["alautun_rest"]*460800000000
+
+        return kin + winal + tun + katun + baktun + piktun + kalabtun +\
+               kinchiltun + alautun + alautun_rest
+
 
 #TEST
 # print("base10toN")
@@ -39,3 +54,21 @@ class Base(object):
 # print("baseNto10")
 # print("Base 20: ", Base.baseNto10(60, 20))
 # print("Base 18: ", Base.baseNto10(40, 18))
+
+# mlc = {"kin":12, "winal":10, "tun":6, "katun":0, "baktun":13, "piktun":0,
+#            "kalabtun":0, "kinchiltun":0, "alautun":0, "alautun_rest":0}
+
+# kin = mlc["kin"]
+# winal = mlc["winal"]
+# tun = mlc["tun"]
+# katun = mlc["katun"]
+# baktun = mlc["baktun"]
+# piktun = mlc["piktun"]
+# kalabtun = mlc["kalabtun"]
+# kinchiltun = mlc["kinchiltun"]
+# alautun = mlc["alautun"]
+# alautun_rest = mlc["alautun_rest"]
+
+# print(f"Mayan LC {alautun_rest}.{alautun}.{kinchiltun}.{kalabtun}.{piktun}.\
+# {baktun}.{katun}.{tun}.{winal}.{kin}\n\
+# Decimal Value is : ", Base.mlctobase10(mlc))
